ff21753a03a8f05affb10268af665e8a3678bb18
allanpscheidt <82116369+allanpscheidt@users.noreply.github.com>
2024-06-12 18:57:45 -0300

    Update game.js

--- game.js
+++ game.js
@@ -1,3 +1,22 @@
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 0 }
+        }
+    },
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+const game = new Phaser.Game(config);
+
 let player;
 let cursors;
 let trashGroup;
